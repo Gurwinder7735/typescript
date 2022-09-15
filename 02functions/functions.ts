d6b143312d971f441c64dd8311aedd3c4d1fa0cb
Gurwinder Singh <gurwindersingh@cqlsys.co.uk>
2022-09-15 09:47:50 +0530

    done

--- 02functions/functions.ts
+++ 02functions/functions.ts
@@ -1,6 +1,7 @@
-function addTwo(num : number){
+function addTwo(num : number): number{
     // num.toUpperCase()
      return num + 2
+    // return "hello"
 }
 
 function getUpper(val: string){
@@ -14,6 +15,32 @@ function signUpUser(name : string, email: string, isSubscribed: boolean){
 function loginUser(name: string, email: string, deviceToken: string = ""){
      
 }
+
+function getValue(value: number): number|string{
+    if(value > 5){
+        return value
+    }else{
+        return "200k"
+    }
+}
+
+const getHello = (s: string): string => {
+       return ""
+}
+
+const consoleError = (err: string): void => {
+    console.log("Here comes the error")
+}
+
+const handleError = (err : string) : never => {
+    throw new Error(err)
+}
+
+const pandavs = ["arjun", "bheem","nakul"]
+
+pandavs.map((pan) => {
+     return `Here is a ${pan}`
+})
 
 
 addTwo(3);
